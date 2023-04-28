01fd02120312849274ebaaea875dac5db663fce9
ziurgabriella <gabriellaruiz907@gmail.com>
2023-04-27 20:30:44 -0400

    added DOM elements

--- quiz/script.js
+++ quiz/script.js
@@ -1,8 +1,8 @@
 // Questions and Answers
 
-var questions = [
-    {
-        prompt: "Commonly used data types DO Not Include:",
+var questions = [    {       
+    
+        prompt: "Commonly used data types DO Not Include:",    
         options: ["strings", "booleans", "alerts", "numbers"],
         answer: "booleans"
     },
@@ -11,5 +11,26 @@ var questions = [
         prompt: "The condition in an if / else statement is enclosed with ______.?",
         options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
         answer: "parenthesis"
+    },
+   
+    {
+        prompt: "Arrays in JavaScript can be used to store _______.",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+
+    {
+        prompt: "String values must be enclosed within _______ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "curly brackets" 
     }
-]
+];
+
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#timer");
+var choicesEl = document.querySelector("#options");
+var submitBtn = document.querySelector("#submit-score");
+var startBtn = document.querySelector("#start");
+var nameEl = document.querySelector("#name");
+var feedbackEl = document.querySelector("#feedback");
+var reStartBtn = document.querySelector("#restart");
