3468a96bf609dfc296a6b139e832b415a2793551
ziurgabriella <gabriellaruiz907@gmail.com>
2023-04-27 20:11:42 -0400

    began creating jsfile and code

---
+++ quiz/script.js
@@ -0,0 +1,15 @@
+// Questions and Answers
+
+var questions = [
+    {
+        prompt: "Commonly used data types DO Not Include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "booleans"
+    },
+
+    {
+        prompt: "The condition in an if / else statement is enclosed with ______.?",
+        options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "parenthesis"
+    }
+]
